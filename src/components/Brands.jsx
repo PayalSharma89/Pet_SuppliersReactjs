5d699336f59a14c3625e7d78de5943e11ba2a7e8
eshazeroit <esha.zeroit@gmail.com>
2025-05-02 12:41:37 +0530

    new change

--- src/components/Brands.jsx
+++ src/components/Brands.jsx
@@ -5,6 +5,7 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 
+
 const Brands = () => {
   const brandImages = [
     { src: "src/assets/images/Brand1.webp", alt: "Brand 1" },
